9096d394b3409c676544192d0cad58c3e055166d
Cih <angel08012000@gmail.com>
2021-03-10 14:38:34 +0800

    Initial Commit

---
+++ poker/pokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokerApp.swift
+//  poker
+//
+//  Created by 慈慈 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct pokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
